a4ae5fd646cd7404a63c017ddc2ba802efa463b0
Felipe Ignacio Castillo Muñoz <pipe9.0@hotmail.cl>
2023-11-19 12:25:09 -0300

    borré un par de espacios

--- linea6.py
+++ linea6.py
@@ -5,16 +5,13 @@ from bokeh.models import  Slider
 from bokeh.layouts import column, row
 from bokeh.models.annotations.labels import Label
 
-
 plot = figure(title="Ciclo otto",x_axis_label='Volumen $$(m^3)$$',y_axis_label='Presión (kPa)')
-
 
 presion_max = Slider(title="Presion máxima (Pa)", value=35*10**6 , start=3*10**6, end=35*10**6, step=10*10**3)
 presion_min = Slider(title="Presion mínima (Pa)", value=2.5*10**6, start=2*10**6, end=3.5*10**6, step=10*10**3)
 volumen = Slider(title="Volumen $$m^3$$", value=1.0, start=0.1, end=10, step=0.1)
 radio_compresion = Slider(title="Radio de compresión", value=8.5, start=2, end=10, step=0.1)
 gamma = 1.4
-
 
 def actualizar_grafico(attrname, old, new):
     # Limpiar el grafico
@@ -53,19 +50,9 @@ def actualizar_grafico(attrname, old, new):
     vd=100*[v1]
     plot.line(vd,pd/1000,legend_label="Proceso 4-1 (isocorico)",line_width=2,color="brown")
 
-
-
     # Calcular eta
     mostrar_eta = 1 - (1 / radio_compresion.value ** (gamma - 1))
-
-
     plot.title.text = "Ciclo Otto - $$\eta =$$  " + str(mostrar_eta)
-
-
-
-    
-
-
 
 #Inicializar grafico con valores por defecto
 presion_max.value = 35*10**6
@@ -73,8 +60,6 @@ presion_min.value = 2*10**6
 volumen.value = 1.0
 radio_compresion.value = 5.0
 actualizar_grafico(None, None, None)
-
-
 
 for w in [presion_max, presion_min, volumen, radio_compresion]:
     w.on_change('value', actualizar_grafico)
